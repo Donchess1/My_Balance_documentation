c8375c415da3ce0f03a600109b2a7e4f969a9875
Donchess1 <stepabod@yahoo.com>
2025-03-06 00:17:32 +0100

    trial phases

--- Mybalance-site/docusaurus.config.ts
+++ Mybalance-site/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Donchess1.github.io',
+  url: 'https://mybalance.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/My_Balance_documentation/',
